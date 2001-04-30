ea497c3b7b3c8cabe1e1aab39bcc01e26a75110f
grrrrr <grrrrr>
2001-04-30 04:57:52 +0000

    added frequent flushing to the PipeSocket threads.  need to re-test http

--- owch/PipeSocket.java
+++ owch/PipeSocket.java
@@ -42,8 +42,8 @@ public class PipeSocket {
             ci = new BufferedInputStream(uc.getInputStream());
             oo = new BufferedOutputStream(oc.getOutputStream());
             co = new BufferedOutputStream(uc.getOutputStream());
-            uc.setSoTimeout(1000 * 15 * 60 * 60); //15 minutes?
-            oc.setSoTimeout(1000 * 15 * 60 * 60); //15 minutes?
+            uc.setSoTimeout(200); //2 sec flushing
+            oc.setSoTimeout(200); 
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -52,8 +52,8 @@ public class PipeSocket {
 
     public void spin() {
         tg = new ThreadGroup("TG:" + label);
-        new PipeThread(uc, oi, co, true, "PTInput:" + label); //
-        new PipeThread(oc, ci, oo, true, "PTOutput:" + label); //
+        new PipeThread(uc, oi, co, false, "PTInput:" + label); //
+        new PipeThread(oc, ci, oo, false, "PTOutput:" + label); //
     };
 
     /**
@@ -70,51 +70,57 @@ public class PipeSocket {
         Object pipe;
         final int blocksize = 18 * 1024;
         byte[] buf = new byte[blocksize];
-
+	String name;
         public PipeThread(Object closeable, InputStream i, OutputStream o, boolean terminate, String name) {
             pipe = closeable;
             is = i;
             os = o;
             term = terminate;
-        
+	    this.name=name;
             new Thread(tg, this, name).start();
         };
 
         /** worker thread */
         public void run() {
-            try {
-                while (true) { //spin around indefinitely
-                    for (avail = is.available(); avail > 0; ) { //runs while
-                        //data exists
-                        //to be
-                        //claimed
-                        Env.debug(500, label + " read has available bytes: " + avail);
-                        actual = is.read(buf);
-                        if (actual == -1) {
-                            os.flush();
-                            Env.debug(15, label + " input stream closed " + actual);
-                            if (term) {
-                                os.close();
-                                //close something...
-                                pipe.getClass().getMethod("close",
-                                    new Class[] { }).invoke(pipe,
-                                    new Object[] { });
-                                //interrupt our sister thread... which
-                                //should be asleep
-                                tg.interrupt();
-                            };
-                            return;
-                        };
-                        Env.debug(500, label + " output: " + actual);
-                        os.write(buf, 0, actual);
-                    };
-                    //we avoid blocking in case we need to be
-                    //interrupted by our sister thread.
-                    Thread.currentThread().sleep(100);
-                };
-            } catch (Exception e) {
-                Env.debug(500, label + " closing: " + e.getMessage());
-            }
-        };
-    };
+	    while (!term)
+		{
+		    try{
+			for (avail = is.available(); avail > 0; ) { //runs while
+			    //data exists
+			    //to be
+			    //claimed
+			    Env.debug(500, label + " read has available bytes: " + avail);
+			    actual = is.read(buf);
+			    if (actual == -1) {
+				os.flush();
+				term=true;
+				Env.debug(15, label + " input stream closed " + actual);
+				if (term) {
+				    os.close();
+				    //close something...
+				    pipe.getClass().getMethod("close",
+							      new Class[] { }).invoke(pipe,
+										      new Object[] { });
+				    //interrupt our sister thread... which
+				    //should be asleep
+				    tg.interrupt();
+				};
+				return;
+			    };
+			    Env.debug(500, label + " output: " + actual);
+			    os.write(buf, 0, actual);
+			};
+			//we avoid blocking in case we need to be
+			//interrupted by our sister thread.
+			Thread.currentThread().sleep(100);
+		    } catch (java.io.InterruptedIOException e){
+			try{os.flush(); }catch (IOException e1) {};
+		    } catch (InterruptedException e) {
+			Env.debug(500, name + " closing: " + e.getMessage()); return;
+		    } catch (Exception e) {
+			Env.debug(500, name + " Error - - closing: " + e.getMessage());return;
+		    };
+		}
+	};
+    }
 };

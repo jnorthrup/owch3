5aaf5f827ea4efbd8f58668580fd6c6dedae547a
grrrrr <grrrrr>
2005-06-04 06:30:37 +0000

    no message

---
+++ java/net/sourceforge/owch2/kernel/AgentLifecycle.java
@@ -0,0 +1,17 @@
+package net.sourceforge.owch2.kernel;
+
+/**
+ * @version $Id: AgentLifecycle.java,v 1.1 2005/06/04 06:30:37 grrrrr Exp $
+ * @Author James Northrup
+ * @copyright All Rights Reserved Glamdring Inc.
+ */
+
+public enum AgentLifecycle {
+    DeployNode,
+    UnLink,
+    Clone,
+    Updated,
+    Update,
+    Link,
+    Shutdown;
+}
